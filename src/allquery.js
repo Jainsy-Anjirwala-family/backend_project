1477172985c6135daadac0c51ea2310d5b770c5f
Jainsy-Anjirwala-family <j.acreatordesign@gmail.com>
2024-10-04 21:31:44 -0700

    gms

--- src/allquery.js
+++ src/allquery.js
@@ -3,6 +3,7 @@ import db from './db.js';
 import msg from './msgcode.js';
 const insertQuery = `INSERT INTO persons (pid, pname) VALUES ($1, $2) RETURNING cid;`;
 const updateQuery = `UPDATE persons SET pid = $2, pname = $3 WHERE cid = $1 RETURNING *;`;
+
 const create = async ({pid,pname}) => {
     const result = await db.query(insertQuery, [pid, pname]);
     if(result && _.isArray(result.rows) && result.rows.length > 0){
--- src/backupindex.js
+++ src/backupindex.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { Pool } = require('pg');
-const app = express()
+const app = express();
 const port = 8080
 
 const selectQuery = `SELECT * FROM persons;`;
